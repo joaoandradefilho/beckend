f0c332fa66e38b906b08b2ad078cc8a31941f8ae
Jaumzerah <jbandrade.dev@gmail.com>
2022-06-10 14:28:12 -0300

    rotas imoveis id

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const cors = require('cors')
 const app = express()
 const imoveis = require('./src/data/imoveis.json')
 const destaques = require('./src/data/destaques.json')
+const imovel_01 = require('./src/data/01.json')
+const imovel_02 = require('./src/data/02.json')
+const imovel_03 = require('./src/data/03.json')
+const imovel_04 = require('./src/data/04.json')
+const imovel_05 = require('./src/data/05.json')
+const imovel_06 = require('./src/data/06.json')
+const imovel_07 = require('./src/data/07.json')
+const imovel_08 = require('./src/data/08.json')
+const imovel_09 = require('./src/data/09.json')
 
 
 app.use(cors())
@@ -18,8 +27,40 @@ app.get('/destaques', (req, res) => {
   return res.json(destaques)
 })
 
-app.get('/data', (req, res) => {
-  return res.json(__dirname)
+app.get('/01', (req, res) => {
+  return res.json(imovel_01)
+})
+
+app.get('/02', (req, res) => {
+  return res.json(imovel_02)
+})
+
+app.get('/03', (req, res) => {
+  return res.json(imovel_03)
+})
+
+app.get('/04', (req, res) => {
+  return res.json(imovel_04)
+})
+
+app.get('/05', (req, res) => {
+  return res.json(imovel_05)
+})
+
+app.get('/06', (req, res) => {
+  return res.json(imovel_06)
+})
+
+app.get('/07', (req, res) => {
+  return res.json(imovel_07)
+})
+
+app.get('/08', (req, res) => {
+  return res.json(imovel_08)
+})
+
+app.get('/09', (req, res) => {
+  return res.json(imovel_09)
 })
 
 app.listen(8080, () => {
